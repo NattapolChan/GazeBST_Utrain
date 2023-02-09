6388930b96a409d1c4f7d0811bb84e19c7fc18c0
NattapolChan <66078943+NattapolChan@users.noreply.github.com>
2023-02-09 23:25:41 +0800

    [UPDATE] video testing for Utrain Fullface

--- Utrain_Baseline_FullFace/video_test.py
+++ Utrain_Baseline_FullFace/video_test.py
@@ -7,7 +7,7 @@ from UnsupervisedGaze_model import *
 import math
 import time
 
-openCV_PATH = '/Users/nattapolchanpaisit/GazeBST/Utrain_Baseline_FullFace/OpenCV_Localisation_Model'
+openCV_PATH = '/Users/nattapolchanpaisit/GazeBST/Utrain_Baseline_FullFace/OpenCV_Localisation_Model/'
 eye_cascade_left = cv2.CascadeClassifier(openCV_PATH+'haarcascade_lefteye_2splits.xml')
 eye_cascade_right = cv2.CascadeClassifier(openCV_PATH+'haarcascade_righteye_2splits.xml')
 face_cascade = cv2.CascadeClassifier(openCV_PATH+'haarcascade_face.xml')
@@ -16,7 +16,7 @@ first_read = True
 cap = cv2.VideoCapture(0)
 ret, img = cap.read()
 model = GazeRepresentationLearning_fullface()
-model = torch.load('/Users/nattapolchanpaisit/GazeBST/Utrain_Baseline_FullFace/ADV_UnsupervisedBaseline_log_6/full_log/baseline_epoch=56_loss=4.044334411621094_batch_size=16_weight_decay=0.0001.pt')
+model.load_state_dict(torch.load('/Users/nattapolchanpaisit/GazeBST/Utrain_Baseline_FullFace/Pretrained_Model/adv_baseline_epoch=56_loss=4.044334411621094_batch_size=16_weight_decay=0.0001.pt', map_location=torch.device('cpu')))
 model.eval()
 
 def crop_center(img,cropy,cropx):
@@ -55,33 +55,31 @@ while(ret):
             cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
             face_gray = img_gray[y:y+h,x:x+w]
             face = img[y:y+h,x:x+w]
-            eyes_left = eye_cascade_left.detectMultiScale(face_gray)
-            if len(eyes_left) > 0:
-                cv2.rectangle(img, (x+eyes_left[0][0],y+eyes_left[0][1]), (x+eyes_left[0][0]+eyes_left[0][2], y+eyes_left[0][1] + eyes_left[0][3]), (0,255,0), 2)
-                eyes_input = face[eyes_left[0][1]: eyes_left[0][1] + eyes_left[0][3], eyes_left[0][0]: eyes_left[0][0] + eyes_left[0][2], :]
-                eyes_input = np.transpose(eyes_input, (2, 0, 1))
-                eyes_input = crop_center(eyes_input, 224, 224)
-                eyes_input = (eyes_input - eyes_input.mean(axis=(0,1,2), keepdims=True)) / eyes_input.std(axis=(0,1,2), keepdims=True)
-                if eyes_input.shape != (3,224,224) :
-                    continue
-                eyes_input = np.resize(eyes_input, (1, 3, 224, 224))
-                eyes_input_torch = torch.from_numpy(eyes_input)
-                eyes_input_torch = eyes_input_torch.to(torch.float32)
-                with torch.no_grad():
-                    output = model(eyes_input_torch)
-                    output = np.array(output)
-                    size = 100
-                    point_from = [eyes_input.shape[3]//2, eyes_input.shape[2]//2]
-                    point_to = [eyes_input.shape[3]//2 + size * math.sin(math.pi/180 * output[0][0]) * math.cos(math.pi/180 * output[0][1]), eyes_input.shape[2]//2 + size * math.sin(math.pi/180 * output[0][1])]
-                    if plot_each_frame:
-                        ax.imshow(eyes_input[0,0,:,:])
-                        ax.arrow(point_from[0], point_from[1], point_to[0]-point_from[0], point_to[1]-point_from[1], color="white", linewidth=3)
-                        yaw = str(float(output[0][0]))[:5]
-                        pitch = str(float(output[0][1]))[:5]
-                        ax.set_title(f'{yaw=} {pitch=}')
-                        plt.ion()
-                        plt.show()
-                    cv2.arrowedLine(img, (x + eyes_left[0][0] + int(point_from[0]), y + eyes_left[0][1] + int(point_from[1]) + 15), ( x + eyes_left[0][0]+ int(point_to[0]), y + eyes_left[0][1] + int(point_to[1]) + 15), color = (0,255,0), thickness = 2)
+            eyes_input = face
+            eyes_input = np.transpose(eyes_input, (2, 0, 1))
+            eyes_input = crop_center(eyes_input, 224, 224)
+            eyes_input = (eyes_input - eyes_input.mean(axis=(0,1,2), keepdims=True)) / eyes_input.std(axis=(0,1,2), keepdims=True)
+            if eyes_input.shape != (3,224,224) :
+                continue
+            print("Here")
+            eyes_input = np.resize(eyes_input, (1, 3, 224, 224))
+            eyes_input_torch = torch.from_numpy(eyes_input)
+            eyes_input_torch = eyes_input_torch.to(torch.float32)
+            with torch.no_grad():
+                output = model(eyes_input_torch)
+                output = np.array(output)
+                size = 100
+                point_from = [eyes_input.shape[3]//2, eyes_input.shape[2]//2]
+                point_to = [eyes_input.shape[3]//2 + size * math.sin(math.pi/180 * output[0][0]) * math.cos(math.pi/180 * output[0][1]), eyes_input.shape[2]//2 + size * math.sin(math.pi/180 * output[0][1])]
+                if plot_each_frame:
+                    ax.imshow(eyes_input[0,0,:,:])
+                    ax.arrow(point_from[0], point_from[1], point_to[0]-point_from[0], point_to[1]-point_from[1], color="white", linewidth=3)
+                    yaw = str(float(output[0][0]))[:5]
+                    pitch = str(float(output[0][1]))[:5]
+                    ax.set_title(f'{yaw=} {pitch=}')
+                    plt.ion()
+                    plt.show()
+                cv2.arrowedLine(img, (x + int(point_from[0]), y  + int(point_from[1]) + 15), ( x + int(point_to[0]), y + int(point_to[1]) + 15), color = (0,255,0), thickness = 2)
     cv2.imshow("image", img)
     a = cv2.waitKey(1)
     if a & 0xFF == ord('q'):
