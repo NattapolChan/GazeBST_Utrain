1ee90c42c6e252868e08b392f8823e075cb11393
NattapolChan <66078943+NattapolChan@users.noreply.github.com>
2023-03-04 03:33:31 +0800

    [UPDATE] more pretrained models

--- Utrain_Baseline_Eye/video_test.py
+++ Utrain_Baseline_Eye/video_test.py
@@ -17,7 +17,7 @@ first_read = True
 cap = cv2.VideoCapture(0)
 ret, img = cap.read()
 model = GazeRepresentationLearning()
-model = torch.load(pretrained_PATH+'advers_final_baseline_error=tensor(6.1656, grad_fn=<SumBackward0>).pt')
+model.load_state_dict(torch.load('/Users/nattapolchanpaisit/GazeBST/Utrain_Baseline_Eye/Pretrained_Model/baseline_error_eye=3-2198.pth', map_location=torch.device('cpu')))
 model.eval()
 
 def crop_center(img,cropy,cropx):
@@ -35,8 +35,13 @@ cur_time = 0
 prev_time = 0
 fps = 0
 
-plot_each_frame = False
+plot_each_frame = True
 fig, ax = plt.subplots()
+preprocess = transforms.Compose([
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    transforms.Resize(size=(36,60))
+])
+
 
 while(ret):
     ret, img = cap.read()
@@ -58,31 +63,28 @@ while(ret):
             face = img[y:y+h,x:x+w]
             eyes_left = eye_cascade_left.detectMultiScale(face_gray)
             if len(eyes_left) > 0:
+                rgbPermutation = [2,1,0]
                 cv2.rectangle(img, (x+eyes_left[0][0],y+eyes_left[0][1]), (x+eyes_left[0][0]+eyes_left[0][2], y+eyes_left[0][1] + eyes_left[0][3]), (0,255,0), 2)
                 eyes_input = face[eyes_left[0][1]: eyes_left[0][1] + eyes_left[0][3], eyes_left[0][0]: eyes_left[0][0] + eyes_left[0][2], :]
                 eyes_input = np.transpose(eyes_input, (2, 0, 1))
-                eyes_input = crop_center(eyes_input, 36, 60)
-                eyes_input = (eyes_input - eyes_input.mean(axis=(0,1,2), keepdims=True)) / eyes_input.std(axis=(0,1,2), keepdims=True)
-                if eyes_input.shape != (3,36,60) :
-                    continue
-                eyes_input = np.resize(eyes_input, (1, 3, 36, 60))
-                eyes_input_torch = torch.from_numpy(eyes_input)
+                eyes_input_torch = torch.from_numpy(eyes_input) / 255
                 eyes_input_torch = eyes_input_torch.to(torch.float32)
+                eyes_input_torch = preprocess(eyes_input_torch)
+                eyes_input_torch = eyes_input_torch.view(1, 3, 36, 60)
                 with torch.no_grad():
+                    eyes_input_torch = eyes_input_torch[:, rgbPermutation, :, :]
                     output = model(eyes_input_torch)
                     output = np.array(output)
                     size = 100
-                    point_from = [eyes_input.shape[3]//2, eyes_input.shape[2]//2]
-                    point_to = [eyes_input.shape[3]//2 + size * math.sin(math.pi/180 * output[0][0]) * math.cos(math.pi/180 * output[0][1]), eyes_input.shape[2]//2 + size * math.sin(math.pi/180 * output[0][1])]
                     if plot_each_frame:
-                        ax.imshow(eyes_input[0,0,:,:])
-                        ax.arrow(point_from[0], point_from[1], point_to[0]-point_from[0], point_to[1]-point_from[1], color="white", linewidth=3)
+                        eyes_input_torch = eyes_input_torch.view(1, 3, 36, 60)
+                        a = eyes_input_torch
+                        a = torch.permute(a, (0,2,3,1)) * 0.5 + 0.5
+                        plt.imshow(a[0])
                         yaw = str(float(output[0][0]))[:5]
                         pitch = str(float(output[0][1]))[:5]
-                        ax.set_title(f'{yaw=} {pitch=}')
-                        plt.ion()
+                        plt.title(f'Vertical = {yaw} Horizontal = {pitch}')
                         plt.show()
-                    cv2.arrowedLine(img, (x + eyes_left[0][0] + int(point_from[0]), y + eyes_left[0][1] + int(point_from[1]) + 15), ( x + eyes_left[0][0]+ int(point_to[0]), y + eyes_left[0][1] + int(point_to[1]) + 15), color = (0,255,0), thickness = 2)
     cv2.imshow("image", img)
     a = cv2.waitKey(1)
     if a & 0xFF == ord('q'):
--- Utrain_Baseline_FullFace/video_test.py
+++ Utrain_Baseline_FullFace/video_test.py
@@ -18,11 +18,11 @@ ret, img = cap.read()
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 model = GazeRepresentationLearning_fullface()
-model.load_state_dict(torch.load('/Users/nattapolchanpaisit/GazeBST/Utrain_Baseline_FullFace/Pretrained_Model/adv_Utrain_fullface_error=5.14.pth', map_location=torch.device('cpu')))
+model.load_state_dict(torch.load('/Users/nattapolchanpaisit/GazeBST/Utrain_Baseline_FullFace/Pretrained_Model/adv_Utrain_fullface_error=3.49.pth', map_location=torch.device('cpu')))
 model.eval()
 
 def crop_center(img,cropy,cropx):
-    _, y,x = img.shape
+    _, y, x = img.shape
     startx = x//2-(cropx//2)
     starty = y//2-(cropy//2)    
     return img[:, starty+10:starty+cropy+10,startx:startx+cropx]
@@ -43,6 +43,7 @@ fps = 0
 
 plot_each_frame = False
 fig, ax = plt.subplots()
+histFps = 0
 
 while(ret):
     ret, img = cap.read()
@@ -53,12 +54,14 @@ while(ret):
     cur_time = time.time()
     if cur_time-prev_time > 1:
         prev_time = time.time()
-        print(f'current FPS = {fps}')
+        histFps = fps
         fps = 0
     fps += 1
-
+    timeEach = {}
+    now = time.time()
     if len(faces) > 0:
         for (x,y,w,h) in faces:
+            prev = time.time()
             cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
             face_gray = img_gray[y:y+h,x:x+w]
             face = img[y:y+h,x:x+w]
@@ -71,23 +74,38 @@ while(ret):
             with torch.no_grad():
                 rgbPermutation = [2,1,0]
                 eyes_input_torch = eyes_input_torch[:, rgbPermutation, :, :]
+                timeEach['preProcess'] = time.time() - prev
+                prev = time.time()
                 output = model(eyes_input_torch)
+                timeEach['modelInference'] = time.time() - prev
+                prev = time.time()
                 eyes_input_torch = eyes_input_torch.view(1, 3, 224, 224)
                 output = np.array(output)
                 size = 100
-                point_from = [112, 112]
-                point_to = [112 * math.sin(math.pi/180 * output[0][0]) * math.cos(math.pi/180 * output[0][1]), 112 * math.sin(math.pi/180 * output[0][1])]
+                point_to = [-size * math.sin(math.pi/180 * output[0][0]) * math.cos(math.pi/180 * output[0][1]), size * math.sin(math.pi/180 * output[0][1])]
                 yaw = str(float(output[0][0]))[:5]
                 pitch = str(float(output[0][1]))[:5]
+                timeEach['postProcess'] = time.time() - prev
+                prev = time.time()
                 if plot_each_frame:
                     a = eyes_input_torch
                     a = torch.permute(a, (0,2,3,1)) * 0.5 + 0.5
                     plt.imshow(a[0,:,:,:])
-                    plt.arrow(point_from[0], point_from[1], point_to[0]-point_from[0], point_to[1]-point_from[1], color="white", linewidth=3)
-                    plt.title(f'{yaw=} {pitch=}')
+                    plt.arrow(112, 112, point_to[1], point_to[0], color="red", linewidth=3)
+                    plt.title(f'Vertical : {yaw=} Horizontal : {pitch=}')
                     plt.show()
-                cv2.arrowedLine(img, (x + int(point_from[0]), y  + int(point_from[1])), ( x + int(point_to[0]), y + int(point_to[1])), color = (0,255,120), thickness = 3)
-                cv2.putText(img, f'V : {yaw}   H : {pitch}', org = (0, 50), fontFace = cv2.FONT_HERSHEY_PLAIN, fontScale = 2, color = (200, 200, 100), thickness=2)
+                cv2.arrowedLine(img, (x + w//2, y + h//2), ( x + w//2 + int(point_to[1]), y + h//2 + int(point_to[0])), color = (255, 0 ,0), thickness = 4, tipLength=0.5)
+                cv2.putText(img, f'V : {yaw}   H : {pitch}', org = (5, 50), fontFace = cv2.FONT_HERSHEY_PLAIN, fontScale = 2, color = (255, 0, 0), thickness=2)
+                overlaySize = []
+                if face.shape[0] > 200:
+                    img[70:270, 0:200] = face[h//2 - 100 : h//2 + 100, w//2 -100 : w//2 + 100]
+    timeEach['total'] = time.time() - now
+    count = 0
+    for key in timeEach:
+        cv2.putText(img, f'{key} processed in {str(1000 * timeEach[key])[:5]} ms', org = (5, 290 + count * 30), fontFace = cv2.FONT_HERSHEY_PLAIN, fontScale = 1.5, color = (255, 0, 0), thickness=2)
+        count += 1
+    if histFps > 0:
+        cv2.putText(img, f'FPS : {histFps} s', org = (5, 290 + count * 30), fontFace = cv2.FONT_HERSHEY_PLAIN, fontScale = 2, color = (0, 255), thickness=2)
     cv2.imshow("image", img)
     a = cv2.waitKey(1)
     if a & 0xFF == ord('q'):
